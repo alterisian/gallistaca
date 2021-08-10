35a38c18dedf64afa9afcd48f3d7bba83645c63e
Ian Moss <hello@ianmoss.com>
2021-08-10 17:33:21 +0000

    adding the valid shrink map fixture, and some outline game logic. A good pairing session with Petar

--- lib/gallistaca.rb
+++ lib/gallistaca.rb
@@ -1,3 +1,23 @@
 class Gallistaca
-    
+    @ship_position = [0,5]
+    @map = nil
+    # load in sprites
+
+    # place the ship on the map
+    def initialize(width)
+        @ship_position=[0, floor(width/2)] #is it floor?
+    end
+
+    def collision_detection
+        # make it crash with the wall.
+        # make it crash with an obstacle.
+    end
+
+    def movement_cycle
+        # make it move through the map
+    end
+
+    def input_detected
+        # make it respond to user input
+    end
 end
